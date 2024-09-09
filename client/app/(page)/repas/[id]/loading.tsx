cd8206c7873265897b9a7fdcf7439e348141df01
dobzacode <contact@corentinkittel.fr>
2024-09-09 11:26:58 +0200

    feat(meal-page): skeleton done

---
+++ client/app/(page)/repas/[id]/loading.tsx
@@ -0,0 +1,47 @@
+import { Separator } from '@/components/ui/separator';
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function Loading() {
+  return (
+    <section className="section-px shadow-primary-40 container flex flex-col justify-center gap-sm tablet:flex-row">
+      <div className="relative aspect-square w-full rounded-xs tablet:w-2/3">
+        <Skeleton className="absolute h-full w-full rounded-xs object-cover object-center" />
+      </div>
+      <div className="flex flex-col gap-sm">
+        <section className="flex h-fit items-center gap-md rounded-xs border bg-background p-md">
+          <div className="relative h-3xl w-3xl overflow-hidden rounded-xs">
+            <Skeleton className="absolute h-full w-full rounded-xs object-cover object-center" />
+          </div>
+          <div className="flex flex-col gap-xs">
+            <Skeleton className="h-5 w-32" />
+            <Skeleton className="h-3 w-28" />
+          </div>
+        </section>
+        <section className="flex h-fit flex-col gap-md rounded-xs border bg-background p-md">
+          <div className="flex flex-col gap-xs">
+            <Skeleton className="h-6 w-20" />
+            <Skeleton className="w-30 h-4 rounded-full" />
+          </div>
+          <Separator />
+          <div className="flex flex-col gap-xs">
+            <Skeleton className="h-4 w-20" />
+            <Skeleton className="h-3 w-4/5" />
+          </div>
+          <Separator />
+          <div className="flex flex-col gap-xs">
+            <Skeleton className="h-4 w-20" />
+            <Skeleton className="h-3 w-3/5" />
+            <Skeleton className="mt-sm h-3 w-full" />
+          </div>
+          <Separator />
+          <div className="flex flex-col gap-xs">
+            <Skeleton className="h-4 w-20" />
+            <Skeleton className="h-3 w-3/5" />
+            <Skeleton className="mt-sm h-3 w-full" />
+            <Skeleton className="my-sm aspect-square w-full" />
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+}
