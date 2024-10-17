6cada5ea7a94bc6feb661c2591d396f16da44011
dobzacode <contact@corentinkittel.fr>
2024-10-17 15:47:50 +0200

    fix(logo): remove brandlogo import

--- client/components/ui/header/logo.tsx
+++ client/components/ui/header/logo.tsx
@@ -1,14 +1,9 @@
-import BrandLogo from '@/public/assets/brand/logo.svg';
 import Link from 'next/link';
 
 interface LogoProps {
   className?: string;
 }
 
-export default function Logo({ className }: LogoProps) {
-  return (
-    <Link href="/">
-      <BrandLogo className={className} />
-    </Link>
-  );
+export default function Logo({}: LogoProps) {
+  return <Link href="/"></Link>;
 }
